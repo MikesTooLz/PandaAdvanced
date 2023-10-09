0c8bb196810b8532e56580f8319290d16c81596f
MikesTooLz <github.track@mikestoolz.com>
2023-10-08 21:10:36 -0400

    Update defeat_ap_nag.py

--- defeat_ap_nag.py
+++ defeat_ap_nag.py
@@ -20,6 +20,8 @@ BUS_SPEED        = 500
 DBC_FILE         = '/usr/local/dbc/Model3CAN.dbc'
 SPEED_ADDR_NAME  = 'ID257DIspeed'
 SPEED_STATE_NAME = 'DI_vehicleSpeed'
+AP_ADDR_NAME = 'ID399DAS_status'
+AP_STATE_NAME = 'DAS_autopilotState'
 
 def get_state(addr_name, idx_name = None, idx_val = None):
   target_addr = db.get_message_by_name(addr_name).frame_id
@@ -49,9 +51,6 @@ p.set_can_speed_kbps(BUS_ID, BUS_SPEED)
 while True:
   sleep(randint(MIN_DELAY, MAX_DELAY))
   speed_state = get_state(SPEED_ADDR_NAME)
-
-  if speed_state[SPEED_STATE_NAME] > 0:
-    btn_state = get_state(BTN_ADDR_NAME, BTN_IDX_NAME, BTN_IDX_VAL)
-    btn_state[BTN_STATE_NAME] = BTN_STATE_VALS[val]
-    set_state(BTN_ADDR_NAME, btn_state)
-    val = (val + 1) % len(BTN_STATE_VALS)
+  ap_state = get_state(AP_ADDR_NAME)
+  ap_finalstate = speed_state[AP_STATE_NAME]
+  print(ap_finalstate)
